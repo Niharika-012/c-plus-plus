7453f2f2bf762efa0185170bcafaeb3a62cb29cf
Niharika-012 <87805219+Niharika-012@users.noreply.github.com>
2021-09-30 10:38:05 +0530

    Update task1.cpp

--- practical05/task1.cpp
+++ practical05/task1.cpp
@@ -5,56 +5,56 @@ using namespace std;
 void Char()
 {
   char a='A';
-  cout<<"Char: "<<a<<endl;
+  cout<<"Char:         "<<sizeof(char)<<endl;
   }
   
 void Bool()
 {
   bool a= false;
-  cout<<"Bool: "<<a<<endl;
+  cout<<"Bool:         "<<sizeof(bool)<<endl;
   }
  
 void Short()
 {
   short a=2;
-  cout<<"Short: "<<a<<endl;
+  cout<<"Short:        "<<sizeof(short)<<endl;
 }
 
 void Int()
 {
   int a=1234;
-  cout<<"Int: "<<a<<endl;
+  cout<<"Int:          "<<sizeof(int)<<endl;
   }
   
 void Long()
 {
   long a= 1567890324;
-  cout<<"Long: "<<a<<endl;
+  cout<<"Long:         "<<sizeof(long)<<endl;
   }
   
 void Float()
 {
   float a=23.567f;
-  cout<<"Float: "<<a<<endl;
+  cout<<"Float:        "<<sizeof(float)<<endl;
   }
   
 void Double()
 {
   double a=678.11134100;
-  cout<<"Double: "<<a<<endl;
+  cout<<"Double:       "<<sizeof(double)<<endl;
   }
 
 void long_double()
 {
     long double a=12.675439091;
-  cout<<"LOng Double: "<<a<<endl;
+  cout<<"LOng Double:  "<<sizeof(long double)<<endl;
   }
   
 void wideChar()
 {
   
   wchar_t a = L'z';
-  wcout<<"Wide char: "<<a<<endl;
+  wcout<<"Wide char:  "<<sizeof(wchar_t)<<endl;
   }
   
 
@@ -71,4 +71,5 @@ int main()
   wideChar();
   
   return 0;
+  }
   
