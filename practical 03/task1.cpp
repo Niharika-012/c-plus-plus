f8b4a005e321010758f61a245113502d93d333c2
Niharika-012 <87805219+Niharika-012@users.noreply.github.com>
2021-10-02 15:04:43 +0530

    Create task1.cpp

---
+++ practical 03/task1.cpp
@@ -0,0 +1,11 @@
+/*Task1- How preprocessor reacts when we use 
+#include<stdio.h> instead of #include<iostream> */
+
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    cout<<"Heya!"<<endl;
+
+    return 0;
+}
