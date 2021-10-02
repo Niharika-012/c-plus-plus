08d9f393a783fc8dd6c65174f732d8fadb31c061
Niharika-012 <87805219+Niharika-012@users.noreply.github.com>
2021-10-02 15:13:23 +0530

    Create task3.cpp

---
+++ practical 03/task3.cpp
@@ -0,0 +1,21 @@
+/*Task3- take a char variable and use cin to take
+its value from the user , cout it and it will only return
+one character and loss rest of the data you have entered!*/
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    char a;
+    
+    cout<<"Enter the value: "<<endl;
+    
+    cin>>a;
+    
+    cout<<"The value of a : "<<a<<endl;
+
+
+    return 0;
+}
