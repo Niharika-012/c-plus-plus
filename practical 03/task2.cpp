0e4e5285450d9fb12a8a06e76687006d11903179
Niharika-012 <87805219+Niharika-012@users.noreply.github.com>
2021-10-02 15:07:27 +0530

    Create task2.cpp

---
+++ practical 03/task2.cpp
@@ -0,0 +1,11 @@
+/*Task2- How preprocessor reacts when we use 
+cout but not using the #include<iostream> header file */
+
+
+using namespace std;
+int main()
+{
+    cout<<"Heya!"<<endl;
+
+    return 0;
+}
