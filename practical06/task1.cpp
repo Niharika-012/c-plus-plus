59229d1d5157e1a188a721e01d7bd44e51b85819
Niharika-012 <87805219+Niharika-012@users.noreply.github.com>
2021-10-07 10:31:46 +0530

    Create task1.cpp

---
+++ practical06/task1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+
+int a = 24 ,b = 45;
+string s1 = "ABC" , s2 ="DE";
+
+float c = 3.2 , d = 4.5;
+
+cout<<"(int)+(int)        :"<<a+b<<endl;
+cout<<"(float)+(float)    :"<<c+d<<endl;
+cout<<"(string)+(string)  :"<<s1+s2<<endl;
+
+cout<<"(int)+(string)     :"<<a+s1<<endl;
+
+return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
